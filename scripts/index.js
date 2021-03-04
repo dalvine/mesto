74197b1096c8e54cb90cfad3f8345335e6c8faae
dalvine <dalvine@ya.ru>
2021-03-04 16:42:06 +0300

    feat: добавить попапы добавления фото, попап фотографии, добавить функцию добавления места

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,110 @@
-let page = document.querySelector('.page');
-let editProfileButton = page.querySelector('.author__button-edit');
-let popup = page.querySelector('.popup');
-let formEditAuthor = page.querySelector('.form')
-
-let closePopupButton = popup.querySelector('.form__button-close');
-let fullNameAuthor = page.querySelector('.author__fullname');
-let jobAuthor = page.querySelector('.author__job');
-let fullNameAuthorInput = popup.querySelector('.form__input_content_fullname');
-let jobAuthorInput = popup.querySelector('.form__input_content_job');
-
-function togglePopup() {
-  if (!popup.classList.contains('popup_opened')) {
-    popup.classList.toggle('popup_opened');
-    fullNameAuthorInput.value = fullNameAuthor.textContent;
-    jobAuthorInput.value = jobAuthor.textContent;
+const page = document.querySelector('.page');
+const editProfileButton = page.querySelector('.author__button-edit');
+const formEditAuthor = page.querySelector('.form[name=formAuthor]');
+const fullNameAuthorInput = formEditAuthor.querySelector('.form__input_content_fullname');
+const jobAuthorInput = formEditAuthor.querySelector('.form__input_content_job');
+const formAddPlace = page.querySelector('.form[name=formAddPlace]');
+const namePlaceInput = formAddPlace.querySelector('.form__input_content_name-place');
+const linkPlaceInput = formAddPlace.querySelector('.form__input_content_link-image');
+const addPlaceButton = page.querySelector('.profile__button-add');
+const btnClosePopupWithFormEditAuthor = page.querySelector('.popup__close_content_form-author');
+const btnClosePopupWithAddPlace = page.querySelector('.popup__close_content_form-add-place');
+const fullNameAuthor = page.querySelector('.author__fullname');
+const jobAuthor = page.querySelector('.author__job');
+const popupWithFormEditAuthor = page.querySelector('.popup_content_form-author');
+const popupWithAddPlace = page.querySelector('.popup_content_add-place');
+const placesList = page.querySelector('.places__list');
+const placeTemplate = page.querySelector('#list-place').content;
+const popupPhoto = page.querySelector('.popup_content_photo');
+const btnClosePopupPhoto = popupPhoto.querySelector('.popup__close_content_photo');
+const popupPhotoContentImg = popupPhoto.querySelector('.popup__image');
+const popupPhotoContentCaption = popupPhoto.querySelector('.popup__caption');
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: './images/karachaevsk.jpg'
+  },
+  {
+    name: 'Гора Эльбрус',
+    link: './images/elbrus.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombay.jpg'
+  },
+  {
+    name: 'Сочи',
+    link: './images/sochi.jpg'
+  },
+  {
+    name: 'Смоленск',
+    link: './images/smolensk.jpg'
+  },
+  {
+    name: 'Роза-хутор',
+    link: './images/rosa-hutor.jpg'
   }
-  else {
-    popup.classList.toggle('popup_opened');
-  }
-}
+];
 
 function saveForm(evt) {
   evt.preventDefault();
   fullNameAuthor.textContent = fullNameAuthorInput.value;
   jobAuthor.textContent = jobAuthorInput.value;
-  togglePopup();
+  closePopup(popupWithFormEditAuthor);
 }
 
-editProfileButton.addEventListener('click', togglePopup);
-closePopupButton.addEventListener('click', togglePopup);
+function addPlace(evt) {
+  evt.preventDefault();
+  const newPlace = [{ name: '', link: '' }];
+  newPlace[0].name = namePlaceInput.value;
+  newPlace[0].link = linkPlaceInput.value;
+  addPlaces(newPlace);
+  closePopup(popupWithAddPlace);
+}
+
+function openPopup(popupContent) {
+  popupContent.classList.add('popup_opened');
+}
+
+function closePopup(popupContent) {
+  popupContent.classList.remove('popup_opened');
+}
+
+function addPlaces(arrayWithPlace) {
+  arrayWithPlace.forEach(place => {
+    const placeElement = placeTemplate.querySelector('.place').cloneNode(true);
+    placeElement.querySelector('.place__photo').src = place.link;
+    placeElement.querySelector('.place__photo').alt = place.name;
+    placeElement.querySelector('.place__title').textContent = place.name;
+    placesList.prepend(placeElement);
+    placeElement.querySelector('.place__like').addEventListener('click', evt => {
+      const evtTarget = evt.target;
+      evtTarget.classList.toggle('place__like_active');
+    });
+    placeElement.querySelector('.place__photo').addEventListener('click' , evt => {
+      const evtTarget = evt.target;
+      openPopupPhoto(evtTarget.src, evtTarget.alt);
+    });
+  });
+}
+
+function openPopupPhoto(link, name) {
+  popupPhotoContentImg.src = link;
+  popupPhotoContentImg.alt = name;
+  popupPhotoContentCaption.textContent = name;
+  popupPhoto.classList.add('popup_opened');
+}
+
+
+addPlaces(initialCards);
 formEditAuthor.addEventListener('submit', saveForm);
+editProfileButton.addEventListener('click', () => {
+  openPopup(popupWithFormEditAuthor);
+  fullNameAuthorInput.value = fullNameAuthor.textContent;
+  jobAuthorInput.value = jobAuthor.textContent;
+});
+btnClosePopupWithFormEditAuthor.addEventListener('click', () => closePopup(popupWithFormEditAuthor));
+addPlaceButton.addEventListener('click', () => openPopup(popupWithAddPlace));
+formAddPlace.addEventListener('submit', addPlace);
+btnClosePopupWithAddPlace.addEventListener('click', () => closePopup(popupWithAddPlace));
+btnClosePopupPhoto.addEventListener('click' , () => closePopup(popupPhoto));
